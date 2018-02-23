3774742f19a069e6d71a2144c42c73462a497307
FlavienMedina <flavien_94@hotmail.fr>
2018-02-23 13:38:38 +0100

    DragonBall

---
+++ class.DragonBall.swift
@@ -0,0 +1,22 @@
+enum DBHeroes {
+  case SANGOKU
+  case BEJITA
+  case BEERUS
+  case KAMESENNIN
+}
+
+class DragonBall: Figurine{
+  private let _character: DBHeroes
+
+  var type: String
+
+  func isMoved(){
+    print("Kamé Hamé Ha!")
+  }
+
+  init(_character: DBHeroes){
+    self._character = _character
+    type = "Dragon Ball figurine \(_character)"
+    print("\(self._character) is singing -->\nCHA-LA HEAD CHA-LA Nani ga okite mo kibun wa heno-heno kappa\nCHA-LA HEAD CHA-LA Mune ga pachi-pachi suru hodo Sawagu Genki-Dama --Sparking !")
+  }
+}

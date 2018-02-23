90b9825563c9ee28ecbec13f8ba99251c85ca742
FlavienMedina <flavien_94@hotmail.fr>
2018-02-23 13:32:42 +0100

    Pony correction$

--- class.Pony.swift
+++ class.Pony.swift
@@ -1,9 +1,9 @@
 class Pony: Toy {
  static private var _nb: Int = 1
  var id: Int = _nb
- var type: String = "Pony #"
+ var type: String = "Pony #\(_nb)"
  init() {
-   print ("\(self.type)\(self.id) is singing -->\nDou-double poney, j'fais izi money \nD'où tu m'connais ? Parle moi en billets violets \nDou-double poney, j'fais izi money")
+   print ("\(self.type) is singing -->\nDou-double poney, j'fais izi money \nD'où tu m'connais ? Parle moi en billets violets \nDou-double poney, j'fais izi money")
    Pony._nb += 1
  }
  func isMoved(){
